79aa48528e661dc0a44ee0b19a3ad4b1a0fe70d8
Jigsaw <71344333+Saw8888@users.noreply.github.com>
2022-04-14 01:33:22 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,31 +1,43 @@
 import cv2
+import pygame
+from pygame.locals import *
+import time
 
 lib = 'Map1.png'
 lib2 = 'Map2.png'
-lib3 = []
+lib3 = ()
+coordenatesx = ()
+coordenatesy = ()
 
 Read = list(cv2.imread(lib).astype("int"))
 Read2 = list(cv2.imread(lib2).astype("int"))
 
 counter = 0
 
-for i in range(len(Read)):#y coords
-    for j in range(len(Read[i])):#x coords
-        blue = list(Read[i][j])[0]
-        green = list(Read[i][j])[1]
-        red = list(Read[i][j])[2]
-        blue2 = list(Read2[i][j])[0]
-        green2 = list(Read2[i][j])[1]
-        red2 = list(Read2[i][j])[2]
-        difference = (blue+green+red)-(blue2+green2+red2)
-        lib3.append(difference)
-        if difference <= 10 and difference >= -10:
-            counter+=1
-            print(counter)
+pygame.init()
 
-if counter >= (i*j)*0.75:
+flags = DOUBLEBUF
+screen = pygame.display.set_mode((500,500), flags, 4)
+
+start = time.process_time()
+for y in range(len(Read)):#y coords
+    for x in range(len(Read[y])):#x coords
+        all = list(Read[y][x])[0]
+        all2 = list(Read2[y][x])[0]
+        difference = (all)-(all2)
+        if difference > 10 or difference < -10:
+            counter+=1
+            pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(x, y, 1, 1))
+
+pygame.display.update()
+print(time. process_time() - start)
+
+if counter >= (y * x) * 0.75:
     print('They are similar images')
-    print('They are different by:', str((counter / (i * j)) * 100), '%')
+    print('They are different by only :', str((counter / (y * x)) * 100), '%')
 else:
     print('They are different')
-    print('They are different by:', str((counter / (i * j)) * 100), '%')
+    print('They are different by:', str((counter / (y * x)) * 100), '%')
+
+pygame.display.update()
+input('End')
